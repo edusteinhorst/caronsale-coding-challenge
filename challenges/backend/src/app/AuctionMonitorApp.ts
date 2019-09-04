d751b23c3b2213df13fe4f7d69752cb805f07cf8
Eduardo Steinhorst <steinhorst@gmail.com>
2019-09-04 00:36:18 -0300

    Register components with IoC container.

--- challenges/backend/src/app/AuctionMonitorApp.ts
+++ challenges/backend/src/app/AuctionMonitorApp.ts
@@ -1,20 +1,31 @@
-import {inject, injectable} from "inversify";
-import {ILogger} from "./services/Logger/interface/ILogger";
-import {DependencyIdentifier} from "./DependencyIdentifiers";
 import "reflect-metadata";
+import { inject, injectable } from "inversify";
+import { ILogger } from "./services/Logger/interface/ILogger";
+import { ICarOnSaleClient } from "./services/CarOnSaleClient/interface/ICarOnSaleClient";
+import { IAuctionStats } from "./services/AuctionStats/interfaces/IAuctionStats";
+import { DependencyIdentifier } from "./DependencyIdentifiers";
 
 @injectable()
 export class AuctionMonitorApp {
 
-    public constructor(@inject(DependencyIdentifier.LOGGER) private logger: ILogger) {
+    public constructor(
+        @inject(DependencyIdentifier.LOGGER) private logger: ILogger,
+        @inject(DependencyIdentifier.CAR_ON_SALE_CLIENT) private carOnSaleClient: ICarOnSaleClient,
+        @inject(DependencyIdentifier.AUCTION_STATS) private auctionStats: IAuctionStats) {
     }
 
     public async start(): Promise<void> {
+        this.logger.info(`Auction Monitor started.`);
 
-        this.logger.log(`Auction Monitor started.`);
-
-        // TODO: Retrieve auctions and display aggregated information (see README.md)
-
+        try {
+            const auctions = await this.carOnSaleClient.getRunningAuctions();
+            const stats = this.auctionStats.getStatsFromAuctions(auctions);
+            this.logger.info(`Number of auctions     : ${stats.numAuctions}`);
+            this.logger.info(`Average number of bids : ${stats.avgNumBids.toFixed(2)}`);
+            this.logger.info(`Average progess ratio  : ${(stats.avgProgressRatio * 100).toFixed(2)}%`);
+        } catch (error) {
+            process.exitCode = -1;
+            this.logger.error(error.message);
+        }
     }
-
 }
--- challenges/backend/src/app/main.ts
+++ challenges/backend/src/app/main.ts
@@ -1,8 +1,12 @@
-import {Container} from "inversify";
-import {ILogger} from "./services/Logger/interface/ILogger";
-import {Logger} from "./services/Logger/classes/Logger";
-import {AuctionMonitorApp} from "./AuctionMonitorApp";
-import {DependencyIdentifier} from "./DependencyIdentifiers";
+import { Container } from "inversify";
+import { ILogger } from "./services/Logger/interface/ILogger";
+import { Logger } from "./services/Logger/classes/Logger";
+import { ICarOnSaleClient } from "./services/CarOnSaleClient/interface/ICarOnSaleClient";
+import { CarOnSaleClient } from "./services/CarOnSaleClient/classes/CarOnSaleClient";
+import { AuctionMonitorApp } from "./AuctionMonitorApp";
+import { DependencyIdentifier } from "./DependencyIdentifiers";
+import { IAuctionStats } from "./services/AuctionStats/interfaces/IAuctionStats";
+import { AuctionStats } from "./services/AuctionStats/classes/AuctionStats";
 
 /*
  * Create the DI container.
@@ -15,7 +19,8 @@ const container = new Container({
  * Register dependencies in DI environment.
  */
 container.bind<ILogger>(DependencyIdentifier.LOGGER).to(Logger);
-
+container.bind<ICarOnSaleClient>(DependencyIdentifier.CAR_ON_SALE_CLIENT).to(CarOnSaleClient);
+container.bind<IAuctionStats>(DependencyIdentifier.AUCTION_STATS).to(AuctionStats);
 
 /*
  * Inject all dependencies in the application & retrieve application instance.
